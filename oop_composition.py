d48b1cb881d024280a08567c8ad367b9735281f8
Karlton S <karltonsuits@gmail.com>
2022-10-10 09:07:57 -0400

    Update oop_composition.py
    refactor for readability & wrap in main function

--- oop_composition.py
+++ oop_composition.py
@@ -2,28 +2,25 @@
 #TODO refactor into something like "Economy"
 #TODO more vehicles in vehicles list
 #TODO empty and wrong make check functions
-#TODO change names
 
-
-class mpg_aggregate:
-    mpg_dict = { 
+class Efficiency:
+    data = { 
             "ford": [],
             "honda": [],
             "nissan": []
         }
     
-    def __init__(self,make,mileage): 
-        self.make = make
-        self.mileage = mileage
+    def __init__(self,brand,economy): 
+        self.brand = brand
+        self.economy = economy
         #now I need to load it into the class member
-        self.mpg_dict[make].append(mileage)
+        self.data[brand].append(economy)
     
     #calculate average mpg by pulling from 
-    def calc_average_mpg(self, input_make_str):
-        if input_make_str in self.mpg_dict:
-            make_mpg_list = self.mpg_dict[input_make_str]
-            average_mpg = sum(make_mpg_list) / len(make_mpg_list)
-            return average_mpg
+    def average(self, brand):
+        if brand in self.data:
+            data = self.data[brand]
+            return sum(data) / len(data)
         else: return -1 #if vehicle make isnt in the dictionary
 
 class vehicle: 
@@ -32,32 +29,36 @@ class vehicle:
         self.make = make
         self.model = model
         self.mileage = mileage
-        self.obj_mpg = mpg_aggregate(make,mileage) #this lets us call average mpg for any initialized object with a make type
+        self.obj_mpg = Efficiency(make,mileage) #this lets us call average mpg for any initialized object with a make type
 
     def make_average_mpg(self):
-        return self.obj_mpg.calc_average_mpg(make)
+        return self.obj_mpg.average(self.make)
 #vehicles as a list of vehicle objects
 
+def main():
+    vehicles = [
+        vehicle(
+            make = "ford",
+            model = "fusion",
+            mileage = 40
+        ),
+        vehicle(
+            make = "ford",
+            model = "f150",
+            mileage = 10
+        ),
+        vehicle(
+            make = "honda",
+            model = "crv",
+            mileage = 20
+        )
+    ]
 
-vehicles = [
-	vehicle(
-		make = "ford",
-		model = "fusion",
-		mileage = 40
-	),
-	vehicle(
-		make = "ford",
-		model = "f150",
-		mileage = 10
-	),
-	vehicle(
-		make = "honda",
-		model = "crv",
-		mileage = 20
-	)
-]
-make = "ford"
-print("hello")
+    make = "ford"
+    print("hello")
 
-print(mpg_aggregate.mpg_dict)
-print(vehicles[0].make_average_mpg())
+    print(Efficiency.data)
+    print(vehicles[0].make_average_mpg())
+
+if __name__=='__main__':
+    main() 
